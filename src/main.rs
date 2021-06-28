5da0545a284633e59845b79af718ea183edd93c6
Hugo Peixoto <hugo.peixoto@gmail.com>
2021-06-28 13:47:20 +0100

    Use clap to parse args and clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,41 @@
-use sha2::{Sha256, Digest};
+#[macro_use]
+extern crate clap;
 
+use sha2::{Sha256, Digest};
 use std::io::Write;
 use std::io::Read;
+use clap::{App, Arg};
 
-#[derive(Debug)]
-pub struct MerkleNode {
+#[derive(Debug, Default)]
+struct MerkleNode {
     hash: [u8; 32],
-    left: Option<usize>,
-    right: Option<usize>,
-    depth: usize,
+    children: Vec<usize>,
+}
+
+impl MerkleNode {
+    fn is_leaf(&self) -> bool {
+        self.children.is_empty()
+    }
 }
 
 trait MerkleAsk {
     fn ask(&mut self, node: &MerkleNode) -> bool;
 }
 
-fn pretty_hash(hash: &[u8; 32]) -> String {
-    hash.iter().map(|b| format!("{:02x}", b)).collect()
-}
-
 fn merklify(hashes: &mut Vec<MerkleNode>, start: usize, count: usize) {
+    let step = 2;
     let mut inserted = 0;
-    for i in (0..count).step_by(2) {
-        let mut hash = [0; 32];
-        let has_right = i + 1 < count;
+    for i in (0..count).step_by(step) {
+        let mut node = MerkleNode::default();
 
         let mut hasher = Sha256::new();
-
-        hasher.update(hashes[start + i].hash);
-        if has_right {
-            hasher.update(hashes[start + i + 1].hash);
+        for j in i .. (i + step).min(count) {
+            hasher.update(hashes[start + j].hash);
+            node.children.push(start + j);
         }
 
-        hash.copy_from_slice(&hasher.finalize());
-        hashes.push(MerkleNode {
-            hash,
-            left: Some(start + i),
-            right: if has_right { Some(start + i + 1) } else { None },
-            depth: hashes[start + i].depth + 1,
-        });
-
+        node.hash.copy_from_slice(&hasher.finalize());
+        hashes.push(node);
         inserted += 1;
     }
 
@@ -48,78 +44,55 @@ fn merklify(hashes: &mut Vec<MerkleNode>, start: usize, count: usize) {
     }
 }
 
-fn merkle_tree(content: &mut dyn std::io::Read, block_size: u64) -> Vec<MerkleNode> {
+fn chunk_hashes(content: &mut dyn std::io::Read, block_size: u64) -> Vec<MerkleNode> {
     let mut hashes = vec![];
 
     loop {
         let mut chunk = Vec::with_capacity(block_size as usize);
         match content.take(block_size).read_to_end(&mut chunk) {
             Err(_) | Ok(0) => {
-                let c = hashes.len();
-                merklify(&mut hashes, 0, c);
                 return hashes;
             },
             Ok(_) => {
-                let mut hash: [u8; 32] = [0; 32];
-                hash.copy_from_slice(&Sha256::digest(&chunk));
-                hashes.push(MerkleNode { hash, left: None, right: None, depth: 0 });
+                let mut node = MerkleNode::default();
+                node.hash.copy_from_slice(&Sha256::digest(&chunk));
+                hashes.push(node);
             }
         }
     }
 }
 
-struct StdinAsker {
+fn merkle_tree(content: &mut dyn std::io::Read, block_size: u64) -> Vec<MerkleNode> {
+    let mut hashes = chunk_hashes(content, block_size);
+
+    let count = hashes.len();
+    merklify(&mut hashes, 0, count);
+    hashes
 }
 
-impl MerkleAsk for StdinAsker {
-    fn ask(&mut self, node: &MerkleNode) -> bool {
-        println!("test {}", pretty_hash(&node.hash));
-        print!("match? ");
-        std::io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        input == "y\n"
-    }
-}
-
-pub fn merkle_print(tree: &Vec<MerkleNode>, idx: usize, indent: usize) {
-    println!("{:indent$}{} {}", "", tree[idx].depth, pretty_hash(&tree[idx].hash), indent=indent);
-
-    if let Some(idx) = tree[idx].left {
-        merkle_print(tree, idx, indent + 2);
-    }
-    if let Some(idx) = tree[idx].right {
-        merkle_print(tree, idx, indent + 2);
-    }
-}
-
-fn merkle_diff(tree: &Vec<MerkleNode>, asker: &mut dyn MerkleAsk) -> Vec<usize> {
+fn merkle_diff(tree: &Vec<MerkleNode>, asker: &mut dyn MerkleAsk) -> (Vec<usize>, usize) {
     let mut blocks = vec![];
+    let mut questions = 0;
     let mut queue = std::collections::VecDeque::new();
 
     queue.push_back(tree.len() - 1);
     while !queue.is_empty() {
         let current = queue.pop_front().unwrap();
-        if let Some(idx) = tree[current].left {
+
+        for &idx in tree[current].children.iter() {
+            questions += 1;
             if !asker.ask(&tree[idx]) {
-                queue.push_back(idx);
+                if tree[idx].is_leaf() {
+                    blocks.push(idx);
+                } else {
+                    queue.push_back(idx);
+                }
             }
         }
 
-        if let Some(idx) = tree[current].right {
-            if !asker.ask(&tree[idx]) {
-                queue.push_back(idx);
-            }
-        }
-
-        if tree[current].left.is_none() && tree[current].right.is_none() {
-            blocks.push(current)
-        }
     }
 
-    blocks
+    (blocks, questions)
 }
 
 struct NetworkAsker {
@@ -133,38 +106,81 @@ impl MerkleAsk for NetworkAsker {
         self.conn.flush().unwrap();
         self.conn.read_exact(&mut answer).unwrap();
 
-        // println!("sent {}, received {}", pretty_hash(&node.hash), pretty_hash(&answer));
-
         answer == node.hash
     }
 }
 
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
-    let filename = &args[1];
-    let server = args[2] == "-s";
-    let address = &args[3];
+    let matches = App::new("merklederp")
+        .version("0.1.0")
+        .author("Hugo Peixoto <hugo.peixoto@gmail.com>")
+        .about("Compares two files over the network")
+        .arg(
+            Arg::with_name("filename").index(1)
+                .help("The filename to compare"),
+        )
+        .arg(
+            Arg::with_name("server").short("s").long("server")
+                .value_name("ADDRESS")
+                .takes_value(true)
+                .conflicts_with("client")
+                .help("listening network address")
+        )
+        .arg(
+            Arg::with_name("client").short("c").long("client")
+                .value_name("ADDRESS")
+                .takes_value(true)
+                .conflicts_with("server")
+                .help("destination network address"),
+        )
+        .arg(
+            Arg::with_name("block_size").short("b").long("block-size")
+                .value_name("BYTES")
+                .takes_value(true)
+                .default_value("1048576")
+                .help("chunk size in bytes"),
+        )
+        .arg(
+            Arg::with_name("verbose").short("v").long("verbose")
+                .takes_value(false)
+                .help("increase verbosity"),
+        )
+        .get_matches();
 
-    println!("checking {}", filename);
-    let mut f = std::fs::File::open(filename).unwrap();
+    let verbose = matches.is_present("verbose");
 
-    println!("building tree...");
-    let tree = merkle_tree(&mut f, 1024*1024);
-    // merkle_print(&tree, tree.len() - 1, 0);
-    println!("done. ({} nodes)", tree.len());
-
-    let conn = if server {
-        std::net::TcpListener::bind(address).unwrap().accept().unwrap().0
-    } else {
-        std::net::TcpStream::connect(address).unwrap()
+    let mut file = match matches.value_of("filename") {
+        Some(filename) => {
+            if verbose { println!("comparing {}", filename); }
+            std::fs::File::open(filename).unwrap()
+        },
+        None => panic!("You must specify a filename"),
     };
 
-    let blocks = merkle_diff(&tree, &mut NetworkAsker{ conn });
+    let conn = if let Some(address) = matches.value_of("server") {
+        std::net::TcpListener::bind(address).unwrap().accept().unwrap().0
+    } else if let Some(address) = matches.value_of("client") {
+        std::net::TcpStream::connect(address).unwrap()
+    } else {
+        panic!("");
+    };
+
+    if verbose { eprintln!("building tree...") };
+    let tree = merkle_tree(&mut file, value_t!(matches, "block_size", u64).unwrap());
+    if verbose { eprintln!("done. ({} nodes)", tree.len()) };
+
+    // merkle_print(&tree, tree.len() - 1, 0);
+
+    let (blocks, questions) = merkle_diff(&tree, &mut NetworkAsker{ conn });
+
+    if verbose { eprintln!("made {} exchanges", questions) };
 
     if !blocks.is_empty() {
-        println!("mismatched blocks:");
+        if verbose { println!("mismatched blocks:") };
         for block in blocks {
             println!("{}", block);
         }
+
+        std::process::exit(1);
     }
 }
